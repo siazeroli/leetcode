ab0a309fb4c102d1b305c3feb189aaaa8c3cfb1f
Xiling Li <xil226@ucsd.edu>
2016-06-23 20:58:39 +0800

    fix two sum problem

--- java/TwoSum.java
+++ java/TwoSum.java
@@ -18,13 +18,18 @@ The return format had been changed to zero-based indices. Please read the above 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] indices = new int[2];
+        boolean find = false;
         
-        for(int i = 0; i < nums.length-1; i++){
-          if(nums[i]+nums[i+1] == target){
-              indices[0] = i;
-              indices[1] = i + 1;
-              break;
-          }
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    indices[0] = i;
+                    indices[1] = j;
+                    find = true;
+                }
+                if(find){break;}
+            }
+            if(find){break;}
         }
         
         return indices;
