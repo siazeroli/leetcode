eca5fb5240430104377bc0d7545e5af259e3092b
Xiling Li <xil226@ucsd.edu>
2017-03-05 19:30:48 -0800

    101. Symmetric Tree

---
+++ java/101SymTree.java
@@ -0,0 +1,66 @@
+/*
+101. Symmetric Tree
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        
+        if(root == null){
+            return true;
+        }
+        
+        List<TreeNode> tq = new ArrayList<TreeNode>();
+        tq.add(root.left);
+        tq.add(root.right);
+        
+        while(tq.size() > 0){
+            TreeNode lt = tq.remove(0);
+            TreeNode rt = tq.remove(0);
+            
+            if(lt == null & rt == null){
+                continue;
+            }
+            else if(lt == null || rt == null){
+                return false;
+            }
+            
+            if(lt.val != rt.val){
+                return false;
+            }
+            
+            tq.add(lt.left);
+            tq.add(rt.right);
+            tq.add(lt.right);
+            tq.add(rt.left);
+        }
+        
+        return true;
+    }
+}
