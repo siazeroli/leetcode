b1d8370e347c365abd2bf012f5a48079aa4c8fd4
Xiling Li <xil226@ucsd.edu>
2017-03-22 14:00:22 -0700

    134. Gas Station

---
+++ java/134GasStation.java
@@ -0,0 +1,46 @@
+/*
+134. Gas Station
+
+There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+
+Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+
+Note:
+The solution is guaranteed to be unique.
+*/
+
+public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int gasSum = IntStream.of(gas).sum();
+        int costSum = IntStream.of(cost).sum();
+        
+        if(gasSum < costSum){
+            return -1;
+        }
+        
+        int total = 0;
+        int index = 0;
+        
+        for(int i = 0; i < gas.length; i++){
+            int cur = gas[i] - cost[i];
+            
+            if(total + cur < 0){
+                if(i < gas.length - 1){
+                    index = i + 1;
+                }
+                else{
+                    index = 0;
+                }
+                
+                total = 0;
+            }
+            else{
+                total += cur;
+            }
+        }
+        
+        return index;
+    }
+}
