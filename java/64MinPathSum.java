6b844b5b4a29825e8cb86d9b668a10de51534666
Xiling Li <xil226@ucsd.edu>
2017-03-23 22:32:35 -0700

    optimize 64

--- java/64MinPathSum.java
+++ java/64MinPathSum.java
@@ -66,3 +66,26 @@ public class Solution {
         return grid[grid.length - 1][grid[0].length - 1];
     }
 }
+
+// Optimize with built-in Math.min to find minimum
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(i == 0 && j > 0){
+                    grid[i][j] += grid[i][j-1];
+                }
+                
+                if(j == 0 && i > 0){
+                    grid[i][j] += grid[i-1][j];
+                }
+                
+                if(i > 0 && j > 0){
+                    grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1]);
+                }
+            }
+        }
+        
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+}
